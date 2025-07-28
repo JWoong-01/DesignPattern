e30285775e187954d22b4c156c1fe789eef8dd0d
Woong <yju407@gmail.com>
2025-07-28 15:06:13 +0900

    퍼사드, 플라이웨이트

---
+++ src/Facade/beforeClient.java
@@ -0,0 +1,39 @@
+package Facade;
+
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class beforeClient {
+
+    public static void main(String[] args) {
+        String to = "keesun@whiteship.me";
+        String from = "whiteship@whiteship.me";
+        String host = "127.0.0.1";
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Test Mail from Java Program");
+            message.setText("message");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+//이메일 Sender -> Settings 사용
+//이메일 Message -> 메시지 정보 지정
+//이메일 Settings -> host 정보
